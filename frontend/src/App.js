9f1eb48d4133db45324779c39f86ad2002f6d649
Sneha Butani <snehabutani1407@gmail.com>
2026-03-04 12:11:25 +0530

    connected frontend to live backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -371,7 +371,7 @@ const PredictionForm = () => {
         Number(formData.active)
       ];
 
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+     const response = await fetch("https://cardio-ml-7ynv.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ data: values }),
